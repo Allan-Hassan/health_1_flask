db8f8dce32107dcd9fb635fb35b789d2888aacba
Allan-Hassan-1080168 <alan1998hassan@gmail.com>
2023-11-03 22:56:54 +0100

    Created registration functionality

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, url_for, redirect, request, render_template
 from flask_bootstrap import Bootstrap5
 from dotenv import load_dotenv
-# from flask_sqlalchemy import SQLAlchemy
 import os
-from classes.models import Category, Product, db
+from classes.models import Category, Product, db, Role, User
+
+from passlib.hash import sha256_crypt
 
 load_dotenv()
 
@@ -34,15 +35,34 @@ def show_login_page():
 
 @app.route('/login', methods = ['POST'])
 def login():
-    # username = request.form['username']
-    # password = request.form['password']
-    # if username == 'admin' and password == 'admin':
-    #     return redirect(url_for('home'))
-    # else:
-    #     return redirect(url_for('show_login_page'))
+    email = request.form['email']
+    password = request.form['password']
     pass
+
 @app.route('/register')
 def show_register_page():
     return render_template('auth/guest/register.html')
+
+@app.route('/register', methods = ['POST'])
+def register():
+    name = request.form['name']
+    email = request.form['email']
+    password = request.form['password']
+    password_confirmation = request.form['password_confirmation']
+
+    if not name or not email or not password: # Check if the fields are empty
+        return redirect(url_for('show_register_page'))
+    
+    if password != password_confirmation:
+        return redirect(url_for('show_register_page'))
+    
+    user = User.query.filter_by(email = email).first() # Check if the email already exists
+    if user:
+        return redirect(url_for('show_register_page'))
+    hashed_password = sha256_crypt.hash(password) # Hash the password
+    new_user = User(name = name, email = email, password = hashed_password, role_id = 3)
+    db.session.add(new_user) # Add the new user to the database
+    db.session.commit() # Commit the changes
+    return redirect(url_for('show_login_page'))
 if __name__ == '__main__':
     app.run(debug = True)
