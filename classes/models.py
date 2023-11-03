eb3c877bca913b9795b753617c131860c82bbee3
Allan-Hassan-1080168 <alan1998hassan@gmail.com>
2023-11-03 22:58:14 +0100

    Added a user model and the relation of the role of it

--- classes/models.py
+++ classes/models.py
@@ -26,4 +26,15 @@ class Role(db.Model):
     level = db.Column(db.Integer, nullable = False)
 
     def __repr__(self): # This method is for debugging purposes
-        return f"Role('{self.name}')"    
+        return f"Role('{self.name}')"
+    
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(50), nullable = False)
+    email = db.Column(db.String(50), nullable = False)
+    password = db.Column(db.String(100), nullable = False)
+    role_id = db.Column(db.Integer, db.ForeignKey('role.id'), nullable = False)
+    role = db.relationship('Role', backref = db.backref('users', lazy = True))
+
+    def __repr__(self): # This method is for debugging purposes
+        return f"User('{self.name}', '{self.email}', '{self.password}', '{self.role_id}')"    
