e71751db7cd872d285389f380312af461e8b1b96
Allan-Hassan-1080168 <alan1998hassan@gmail.com>
2023-11-03 19:39:26 +0100

    Created a product class

--- classes/models.py
+++ classes/models.py
@@ -8,3 +8,14 @@ class Category(db.Model):
 
     def __repr__(self): # This method is for debugging purposes
         return f"Category('{self.name}', '{self.picture}')"
+    
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(50), nullable = False)
+    picture = db.Column(db.String(100), nullable = False)
+    description = db.Column(db.String(500), nullable = False)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable = False)
+    category = db.relationship('Category', backref = db.backref('products', lazy = True))
+
+    def __repr__(self): # This method is for debugging purposes
+        return f"Product('{self.name}', '{self.picture}', '{self.description}', '{self.price}', '{self.category_id}')"    
